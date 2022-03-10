e66be10bca509d9b1e220f715b42f7c312ae8e0f
franmb93 <fcomunozbetanzos@gmail.com>
2022-03-10 12:53:12 +0100

    Fixed some imports

--- src/main/java/com/example/bottegaBack/controllers/UserController.java
+++ src/main/java/com/example/bottegaBack/controllers/UserController.java
@@ -6,7 +6,6 @@ import com.example.bottegaBack.DTO.UserDTO;
 import com.example.bottegaBack.entities.User;
 import com.example.bottegaBack.services.IUserService;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- src/main/java/com/example/bottegaBack/entities/User.java
+++ src/main/java/com/example/bottegaBack/entities/User.java
@@ -1,14 +1,10 @@
 package com.example.bottegaBack.entities;
 
-import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
