90278ca69a7370b6c81b8c3e81d036f4661e3916
franmb93 <fcomunozbetanzos@gmail.com>
2022-03-10 10:37:19 +0100

    Skeleton - first commit

---
+++ src/main/java/com/example/bottegaBack/BottegaBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.bottegaBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BottegaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BottegaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bottegaBack/BottegaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bottegaBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BottegaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
